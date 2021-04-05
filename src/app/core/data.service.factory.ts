5819aca8bfea017e203738e410b97a4d72eb93d3
Brice Wilson <brice@5wilsons.net>
2021-04-05 10:40:43 -0400

    updated data.service.factory params

--- src/app/core/data.service.factory.ts
+++ src/app/core/data.service.factory.ts
@@ -1,13 +1,14 @@
-// import { LoggerService } from './logger.service';
-// import { DataService } from './data.service';
+import { LoggerService } from './logger.service';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
 
-// export function dataServiceFactory(logger: LoggerService) {
-//   let dataService: DataService = new DataService(logger);
+export function dataServiceFactory(logger: LoggerService, http: HttpClient) {
+  let dataService: DataService = new DataService(logger, http);
 
-//   // do more stuff to configure the service if necessary
+  // do more stuff to configure the service if necessary
 
-//   logger.log('Creating a new data service with a factory function.');
+  logger.log('Creating a new data service with a factory function.');
 
-//   return dataService;
+  return dataService;
   
-// }
+}
